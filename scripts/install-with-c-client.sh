2b76cc30cf2484c636d9a9c20b5ad6fb0990ac12
Tom Leasure <tleasure@gmail.com>
2025-12-04 17:52:42 -0500

    Build C client if not available from prebuild

---
+++ scripts/install-with-c-client.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+################################################################################
+# Smart installer that only builds C client when building from source
+################################################################################
+
+# First, try to install pre-built binaries
+node-pre-gyp install --addon_version=$(node -p "require('./package.json').version") 2>/dev/null
+
+# Check if installation succeeded
+if [ $? -eq 0 ]; then
+    echo "Successfully installed pre-built binaries"
+    exit 0
+fi
+
+# Pre-built binaries not available, need to build from source
+echo "Pre-built binaries not available, building from source..."
+
+# Build C client first
+echo "Building Aerospike C client..."
+./scripts/build-c-client.sh || {
+    echo "Warning: C client build failed, attempting to use existing build"
+}
+
+# Now build the Node.js addon
+echo "Building Node.js addon..."
+node-pre-gyp build --addon_version=$(node -p "require('./package.json').version")
+
